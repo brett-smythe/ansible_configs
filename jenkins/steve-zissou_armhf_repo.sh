b431302503e024c2e08d3c4113c7cc7dfc2937cd
brett <brett@debian-laptop>
2016-10-31 00:29:10 -0700

    Added armhf jenkins build files

---
+++ jenkins/steve-zissou_armhf_repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+old_debfile=$(aptly repo search internal-jessie 'steve-zissou {armhf}')
+if [ "$old_debfile" != "" ]; then
+    (aptly repo remove internal-jessie $old_debfile || : )
+fi
+debfile=$(find /var/lib/jenkins/armhf_build/steve-zissou/ -maxdepth 1 -name *\.deb*)
+(aptly repo add internal-jessie $debfile || : )
+(aptly snapshot create $debfile-stable from repo internal-jessie || : )
+(aptly publish drop jessie || : )
+aptly publish snapshot -architectures=amd64,armhf,i386 $debfile-stable
+rm $debfile
